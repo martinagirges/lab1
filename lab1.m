98f3e58554bf5b9b6f12d922d063f4fc9dee3f10
martinagirges <93275915+martinagirges@users.noreply.github.com>
2021-10-30 13:55:17 +0200

    Add files via upload

---
+++ lab1.m
@@ -0,0 +1,28 @@
+%% Q1 
+A =[-7 5 -9; 2 -1 2; 1 -1 2] 
+B =[16 3 2 13 ;5 10 11 8 ;9 6 7 12 ; 4 15 14 1] 
+C =[4 2 -3; 7 -7 9 ;3 -5 6 ] 
+D = [6 3 2 ;2 12 -7 ; -1 6 2 ; -5 15 11]  
+ a= 3*A - 5*C 
+ d='error because rows and column are not equal'
+ c=C*A 
+ d='error because rows and column are not equal' 
+%% Q2 
+zeros(4) 
+zeros(3,3)
+ones(2)
+ones(4,2)
+size(D)
+zeros(size(D))
+diag([1 2 3 4])
+eye(4)
+%% Q3 
+[4,5] 
+[4;5] 
+
+%% Q4:
+m= zeros(7,8) , m=diag([5 5 5 5 5 5 5 5]) , m(:,8)=5
+
+%% Q5:
+A(3,:)
+A(:,2)
